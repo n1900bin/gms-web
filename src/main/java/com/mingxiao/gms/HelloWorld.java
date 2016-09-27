bfa252ac087c35f7aded3b3c89000b043e5481e7
haoxiaoliang <haoxiaoliang@bjmingxiao.com>
2016-09-27 13:19:16 +0800

    Feature 0001 added

--- src/main/java/com/mingxiao/gms/HelloWorld.java
+++ src/main/java/com/mingxiao/gms/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld
     public static void main(String[] args)
     {
 	System.out.println("This project is created for studying on how to use Git and Maven to mangage a Java Web Dynamic Project.");
+	
+	System.out.println("Feature 0001: output something to indicate that feature 0001 has implemented.");
     }
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello Git and Maven!</h2>
+<h3>Feature 0001 has been added.</h3>
 </body>
 </html>
