5f1465219b0c426a4664214b4d8d3ca6333a8c7f
haoxiaoliang <haoxiaoliang@bjmingxiao.com>
2016-09-27 13:02:45 +0800

    Initialize project

---
+++ src/main/java/com/mingxiao/gms/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mingxiao.gms;
+
+public class HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+	System.out.println("This project is created for studying on how to use Git and Maven to mangage a Java Web Dynamic Project.");
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Git and Maven!</h2>
+</body>
+</html>
